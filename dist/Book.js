d6433afdd5c7ca3e56064062a5c50d5556d992f3
Mathias Gonzalez <mathiasgonzalezb96@gmail.com>
2024-12-19 09:26:56 -0300

    Se agrego el .gitignore y se eliminaron archivos del segumiento

--- dist/Book.js
+++
@@ -1,8 +0,0 @@
-class Book {
-    constructor(title, author, year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-}
-//# sourceMappingURL=Book.js.map
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-// import { Book } from './Book';
-const book1 = new Book('1984', 'George Orwell', 1949);
-const book2 = new Book('Brave New World', 'Aldous Huxley', 1932);
-// console.log(book1.getDetails());
-// console.log(book2.getDetails());
-// console.log(Book.compareByYear(book1, book2));
-//# sourceMappingURL=index.js.map
